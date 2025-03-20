87ba5a8fee7063501cacbb94fa70dffa8d5d7786
Seymanur KARA <seymanurkara@Cybersec-3.local>
2025-03-20 14:13:50 +0100

    endret command

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TodoList from "./components/Todolist.tsx";
-
+import TodoList from "./components/TodoList"; 
 
 
 function App() {
